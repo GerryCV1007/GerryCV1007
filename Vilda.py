57bfed199892b0ef82075ac3aa6e957a51d640b2
GerryCV1007 <gerrycv10@gmail.com>
2025-02-21 07:43:03 +0800

    Add files via upload

---
+++ Vilda.py
@@ -0,0 +1,22 @@
+n = 5
+i = 1
+while i <= n:
+    print(' ' * (n - i) + '*' * (2 * i - 1))
+    i += 1
+n = 5
+i = n
+print("\n")
+while i >= 1:
+    print(' ' * (n - i) + '*' * (2 * i - 1))
+    i -= 1
+n = 5
+i = 1
+print("\n")
+while i <= n:
+    print(' ' * (n - i) + '*' * (2 * i - 1))
+    i += 1
+
+i = n - 1
+while i >= 1:
+    print(' ' * (n - i) + '*' * (2 * i - 1))
+    i -= 1
